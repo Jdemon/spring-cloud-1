0ac193e280609f3867a2fccad926dab0a0824044
Chaiwat <chaiwk97@orcsf.postbox.in.th>
2019-11-22 13:43:06 +0700

    Initial Project

---
+++ pricing-service/src/test/java/th/co/orcsoft/pricing/PricingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package th.co.orcsoft.pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
