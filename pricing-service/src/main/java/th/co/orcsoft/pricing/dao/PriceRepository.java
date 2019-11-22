b7a84de2df847e9faeecbcab4be0b8d7f2941a32
Chaiwat <chaiwk97@orcsf.postbox.in.th>
2019-11-22 16:19:00 +0700

    add config server

--- pricing-service/src/main/java/th/co/orcsoft/pricing/dao/PriceRepository.java
+++ pricing-service/src/main/java/th/co/orcsoft/pricing/dao/PriceRepository.java
@@ -6,21 +6,31 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-@Repository
+@Component
 public class PriceRepository {
 	
+	@Value("${hawaiian_pizza.price}")
+	private String hawaiian_pizza;
+	@Value("${xiaolongbao.price}")
+	private String xiaolongbao;
+	@Value("${kimchi.price}")
+	private String kimchi;
+	@Value("${oolong.price}")
+	private String oolong;
 	
-	private Map<String,BigDecimal> pricingMap = Stream.of(
-			  new AbstractMap.SimpleEntry<>("Hawaiian Pizza", new BigDecimal("300")), 
-			  new AbstractMap.SimpleEntry<>("Xiaolongbao", new BigDecimal("200")),
-			  new AbstractMap.SimpleEntry<>("Kimchi",new BigDecimal("50")),
-			  new AbstractMap.SimpleEntry<>("Oolong tea",new BigDecimal("30"))
-			  ).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-	
+	private Map<String,BigDecimal> pricingMap = null;
 	
 	public BigDecimal findByMenuName(String name) {
+		pricingMap = Stream.of(
+					  new AbstractMap.SimpleEntry<>("Hawaiian Pizza", new BigDecimal(hawaiian_pizza)), 
+					  new AbstractMap.SimpleEntry<>("Xiaolongbao", new BigDecimal(xiaolongbao)),
+					  new AbstractMap.SimpleEntry<>("Kimchi",new BigDecimal(kimchi)),
+					  new AbstractMap.SimpleEntry<>("Oolong tea",new BigDecimal(oolong))
+					  ).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+		
 		return pricingMap.get(name);
 	}
 
